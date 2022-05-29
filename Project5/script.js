672de5cf1811c8a655332cd7f638c343a0da25e5
Sarfaraz45 <56291130+Sarfaraz45@users.noreply.github.com>
2022-05-29 10:14:17 +0500

    updated button click function
    Created button click evens

--- Project5/script.js
+++ Project5/script.js
@@ -48,6 +48,62 @@ updateDOM();
 
 }
 
+//Function to Double Money of all Users
+function doubleMoney(){
+console.log("Old User data", data);
+    //Loop through all users in the user data array
+    //For each user, return the user data
+    //overwrite the data array with the new data array created by map
+
+    data = data.map(user => {
+        return { ...user, balance: user.balance * 2  }
+    });
+
+    console.log("New User daata", data)
+    //Update the DOM using the new user data array
+    updateDOM();
+
+}
+
+
+//Function to filter the Millionaire Users
+
+function filterUsers(){
+    //Filter out all users whose balance is less than million
+  data =  data.filter(user => user.balance > 1000000);
+  // Update the DOM with new User data
+  updateDOM();
+}
+
+//Function to sort Users by balance
+
+function sortByBalance(){
+    //Sort users by balance using a compare function inside sort
+    data.sort((a,b)=>a.balance - b.balance);
+    // Update the DOM with new User data
+    updateDOM();
+}
+
+// Function to sum all user balance into total balance
+function totalBalance(){
+    // Add up all balance from all users
+    // Accumulator starts at 0 and adds the current users balance for each iteration
+
+    const balance = data.reduce((acc, user) => (acc += user.balance), 0);
+
+    //Update DOM
+    updateDOM();
+
+}
+
+
+
+//Function to Format random number as money
+function formatNumberToDollar(number){
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
 //Function to the Update the UI with data from the user data array
 function updateDOM(userData = data){
   //Clear Previous UI
@@ -61,12 +117,34 @@ function updateDOM(userData = data){
      //Apply the user class to the new div
      userDiv.classList.add('user');
         //Add inner HTML to the user div
-    userDiv.innerHTML = `<strong>${user.name}</strong> ${user.balance}`
+    userDiv.innerHTML = `<strong>${user.name}</strong> ${formatNumberToDollar(user.balance)}`
     //Add the new element into the DOM
     main.appendChild(userDiv);
  });
 
 }
+
+
+//Event Listeners
+// 1. Listen For click on Add User
+addUserBtn.addEventListener('click', getRandomUser);
+
+//2. Listen for the click on the Double Button
+
+doubleBtn.addEventListener('click', doubleMoney)
+
+//3. Listen for click on Filter Button
+filterBtn.addEventListener('click', filterUsers);
+
+//4. Listen for click on Sort Button
+sortBtn.addEventListener('click',sortByBalance);
+
+//5. Listen for click on Sum Button
+SumBtn.addEventListener('click', totalBalance);
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
 getRandomUser();
 getRandomUser();
 
